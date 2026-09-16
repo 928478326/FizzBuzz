2b0fcecbb75aaa4fd93a6642168544bc6d0bd6f6
Quhao Sun <sunquhao@outlook.com>
2026-09-15 23:24:58 -0400

    practice calling FizzBuzz methods

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -3,6 +3,6 @@ package lab1;
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFB(5);
-        FizzBuzz.main(new String[0]);
+        FizzBuzz.main(new String[0git add README.md]);
     }
 }
